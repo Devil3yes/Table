d8e8a615c4613dc570845ccda13eb3af83026e91
Devil3yes <83644544+Devil3yes@users.noreply.github.com>
2021-05-15 17:02:18 +0100

    Create

---
+++ Index.vue
@@ -0,0 +1,95 @@
+<template>
+  <q-page padding>
+    <div class="row q-mb-lg">
+      <q-input v-model="add" placeholder="Add new Title" class="col" @keyup.enter="add"/>
+      <q-btn color="primary" size="sm" label="Add" @click.native="add"/>
+    </div>
+    <div class="row justify-evenly">
+      <q-list hight class="col">
+        <q-list-header>Task</q-list-header>
+        <q-item v-font="newTask in add">
+          <q-item-main :label="addTask"/>
+          <q-item-side right icon="Check" color="secondary"/>
+        </q-item>
+      </q-list>
+    </div>
+    <div class="q-pa-md">
+    <q-table
+      title="Table"
+      :data="data"
+      :columns="columns"
+      row-key="name"
+      hide-header
+      hide-bottom/>
+  </div>
+  <q-page-sticky position="bottom-right"  :offset="[45,178]">
+    <q-btn class="btn-add-right" round color="primary" icon="add"/>
+  </q-page-sticky>
+  </q-page>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  name: 'PageIndex',
+  data () {
+    return {
+      framework: {
+        plugins: [
+          'Cookies']
+      },
+      add: [],
+      newTask: '',
+      columns: [
+        {
+          name: 'name',
+          required: true,
+          label: 'Dessert (100g serving)',
+          align: 'left',
+          field: row => row.name,
+          format: val => `${val}`,
+          sortable: true
+        },
+        { name: 'calories', align: 'center', label: 'Calories', field: 'calories', sortable: true },
+        { name: 'fat', label: 'Fat (g)', field: 'fat', sortable: true },
+        { name: 'carbs', label: 'Carbs (g)', field: 'carbs' },
+        { name: 'protein', label: 'Protein (g)', field: 'protein' },
+        { name: 'sodium', label: 'Sodium (mg)', field: 'sodium' }],
+      data: [
+        {
+          name: 'Frozen Yogurt',
+          calories: 159,
+          fat: 6.0,
+          carbs: 24,
+          protein: 4.0,
+          sodium: 87
+        },
+        {
+          name: 'Ice cream sandwich',
+          calories: 237,
+          fat: 9.0,
+          carbs: 37,
+          protein: 4.3,
+          sodium: 129
+        },
+        {
+          name: 'Eclair',
+          calories: 262,
+          fat: 16.0,
+          carbs: 23,
+          protein: 6.0,
+          sodium: 337
+        }
+      ]
+    }
+  },
+  methods: {
+    addTask () {
+      this.add.push(this.newTask)
+      this.newTask = ''
+    }
+  }
+}
+</script>
---
+++ MainLayout.vue
@@ -0,0 +1,92 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        />
+
+        <q-toolbar-title>
+          My Quasar App :3
+        </q-toolbar-title>
+        <div>
+          <q-avatar rounded>
+            <img src="https://avatars.githubusercontent.com/u/83644544?s=60&amp;v=4" class="rounded-circle" size="20" height="20" width="20">
+          </q-avatar>
+        </div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      content-class="bg-grey-1"
+    >
+      <q-list>
+        <q-item-label
+          header
+          class="text-grey-8"
+        >
+          Essential Links
+        </q-item-label>
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import EssentialLink from 'components/EssentialLink.vue'
+
+const linksData = [
+  {
+    title: 'Quasar',
+    caption: 'quasar.dev',
+    icon: 'school',
+    link: 'https://quasar.dev'
+  },
+  {
+    title: 'Github',
+    caption: 'github.com',
+    icon: 'code',
+    link: 'https://github.com/'
+  },
+  {
+    title: 'Icons',
+    caption: 'Make Icons',
+    icon: 'favorite',
+    link: 'https://fonts.google.com/icons?selected=Material+Icons:home&icon.query=+'
+  },
+  {
+    title: 'Table',
+    caption: 'Make Tables',
+    icon: 'table_chart',
+    link: 'https://quasar.dev/vue-components/table#introduction'
+  }
+];
+
+export default {
+  name: 'MainLayout',
+  components: { EssentialLink },
+  data () {
+    return {
+      leftDrawerOpen: false,
+      essentialLinks: linksData
+    }
+  }
+}
+</script>
