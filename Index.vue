564da0b2e9d7efcb43aea19b5cc0c58938963929
Devil3yes <83644544+Devil3yes@users.noreply.github.com>
2021-06-04 16:09:22 +0100

    M3

--- Index.vue
+++ Index.vue
@@ -1,8 +1,8 @@
 <template>
   <q-page padding>
     <div class="row q-mb-lg">
-      <q-input v-model="add" placeholder="Add new Title" class="col" @keyup.enter="add"/>
-      <q-btn color="primary" size="sm" label="Add" @click.native="add"/>
+      <q-input v-model="add" placeholder="Add new Title" class="col"/>
+      <q-btn color="primary" size="sm" label="Add" @click.native="addTask"/>
     </div>
     <div class="row justify-evenly">
       <q-list hight class="col">
@@ -33,12 +33,10 @@ export default {
   name: 'PageIndex',
   data () {
     return {
-      framework: {
-        plugins: [
-          'Cookies']
-      },
       add: [],
+      loading: false,
       newTask: '',
+      rowCount: 10,
       columns: [
         {
           name: 'name',
@@ -51,33 +49,26 @@ export default {
         },
         { name: 'calories', align: 'center', label: 'Calories', field: 'calories', sortable: true },
         { name: 'fat', label: 'Fat (g)', field: 'fat', sortable: true },
-        { name: 'carbs', label: 'Carbs (g)', field: 'carbs' },
-        { name: 'protein', label: 'Protein (g)', field: 'protein' },
-        { name: 'sodium', label: 'Sodium (mg)', field: 'sodium' }],
+        { name: 'years', label: 'Carbs (g)', field: 'carbs' }
+      ],
       data: [
         {
           name: 'Frozen Yogurt',
           calories: 159,
           fat: 6.0,
-          carbs: 24,
-          protein: 4.0,
-          sodium: 87
+          carbs: 24
         },
         {
           name: 'Ice cream sandwich',
           calories: 237,
           fat: 9.0,
-          carbs: 37,
-          protein: 4.3,
-          sodium: 129
+          carbs: 37
         },
         {
           name: 'Eclair',
           calories: 262,
           fat: 16.0,
-          carbs: 23,
-          protein: 6.0,
-          sodium: 337
+          carbs: 23
         }
       ]
     }
@@ -86,8 +77,25 @@ export default {
     addTask () {
       this.add.push(this.newTask)
       this.newTask = ''
+      this.loading = true
+      setTimeout(() => {
+        const
+          index = Math.floor(Math.random() * (this.data.length + 1)),
+          row = this.original[Math.floor(Math.random() * this.original.length)]
+        if (this.data.length === 0) {
+          this.rowCount = 0
+        }
+        row.id = ++this.rowCount
+        const addRow = { ...row } // extend({}, row, { name: `${row.name} (${row.__count})` })
+        this.data = [ ...this.data.slice(0, index), addRow, ...this.data.slice(index) ]
+        this.loading = false
+      }, 500)
+    },
+    removeTask () {
+      self.removeLine = function () {
+        self.koData.remove(this);
+      }
     }
   }
-  
 }
 </script>
